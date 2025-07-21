94f883947a5f2236a0fae05f1b32cdeee18da6d1
sh-mirjalol-3930 <mirjalolshomurodov179@gmail.com>
2025-07-21 18:23:47 +0500

    hometype footer fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <div className="container">
+        <div className="flex justify-between py-[30px] items-baseline">
+          <div className='flex flex-col gap-[10px]'>
+            <h1 className='text-[20px] font-bold'>Funiro.</h1>
+            <p className='text-[#9F9F9F]'>400 University Drive Suite 200 Coral <br /> Gables, <br />
+            FL 33134 USA</p>
+          </div>
+          <ul className='flex flex-col gap-[10px]'>
+            <li className='text-[#9F9F9F]'>Links</li>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          <ul className='flex flex-col gap-[10px]'>
+            <li className='text-[#9F9F9F]'>Help</li>
+            <li>Payment Options</li>
+            <li>Returns</li>
+            <li>Privacy Policies</li>
+          </ul>
+          <div className='flex flex-col gap-[10px]'>
+            <p className='text-[#9F9F9F]'>Newsletter</p>
+            <div className='flex gap-2 items-center'>
+            <input className='border-1 border-[#0000] border-b-gray-500' type="text" placeholder='Enter Your Email Address' />
+            <button className='border-1 border-[#0000] border-b-gray-500 font-bold'>SUBSCRIBE</button>
+            </div>
+          </div>
+        </div>
+      </div>      
+      <div className="container  border-1 border-[#0000] border-t-gray-500 py-[15px]">
+        <b>2023 furino. All rights reverved</b>
+      </div>
+    </footer>
   )
 }
 
--- src/components/home-type/HomeType.jsx
+++ src/components/home-type/HomeType.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import dining from "./1.svg"
+import bedroom from "./bedroom.svg"
+import living from "./living room.svg"
 
 const HomeType = () => {
   return (
-    <div>HomeType</div>
+    <div className='bg-[#F0F0F0]'>
+      <div className='container flex flex-col items-center'>
+        <h1 className='text-center font-bold text-[40px] text-[#3A3A3A] mb-7'>Browse The Range</h1>
+        <p className='text-center text-[20px] text-light pb-[20px]  text-[#3a3a3a]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="flex items-center gap-[50px] mb-[20px] justify-between">
+          <div className="flex flex-col items-center gap-8">
+            <img src={dining} alt="" />
+            <b className='text-[20px] text-[#3a3a3a]'>Dining</b>
+          </div>
+          <div className="flex flex-col items-center gap-8">
+            <img src={living} alt="" />
+            <b className='text-[20px] text-[#3a3a3a]'>Living</b>
+          </div>
+          <div className="flex flex-col items-center gap-8">
+            <img src={bedroom} alt="" />
+            <b className='text-[20px] text-[#3a3a3a]'>Bedroom</b>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
