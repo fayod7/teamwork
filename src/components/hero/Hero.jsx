fe33c1774fc5b64daaf7ae33e0750481e7d66dd9
fayod7 <fayodnv@gmail.com>
2025-07-21 18:04:39 +0500

    header

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
-const Hero = ({text}) => {
+const Hero = () => {
   return (
-    <div className='hero'>{text}</div>
+    <section className='w-full min-h-screen bg-[url(/src/assets/productsBranch/hero.png)] bg-no-repeat bg-cover bg-center'>
+      <div className="container flex items-center justify-end w-full h-screen max-[950px]:justify-center">
+          <div className="bg-[#FFFFFFB2] w-[650px] rounded-lg py-[60px] px-[50px] flex flex-col gap-4">
+            <h2 className='text-[#333333] font-semibold'>New Arrival</h2>
+            <h1 className='text-[#B88E2F] font-bold text-[50px]'>Discover Our New Collection</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+         <div><button className='text-white border border-[#B88E2F] bg-[#B88E2F] py-3 px-16  duration-200 hover:cursor-pointer hover:bg-white hover:text-[#B88E2F] uppercase'>buy now</button></div>
+          </div>
+      </div>
+    </section>
   )
 }
 
