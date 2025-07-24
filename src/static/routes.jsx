fd47326869d6f257fae1484dc8ca988735fb4367
fayod7 <fayodnv@gmail.com>
2025-07-24 17:02:58 +0500

    ready for new pages

---
+++ src/static/routes.jsx
@@ -0,0 +1,22 @@
+const routes = [
+ {
+    id: 5,
+    src: <AiOutlineShoppingCart className="size-6" />,
+    to: "/cart",
+  },
+  {
+    id: 6,
+    src:<FaRegHeart className="size-5" />,
+    to: "/likes",
+  },
+  {
+    id: 7,
+    src:<FaRegHeart className="size-5" />,
+    to: "/search",
+  },
+  {
+    id: 8,
+    src:<FaRegUser className="size-5" />,
+    to: "/user",
+  },
+]
